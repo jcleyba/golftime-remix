6a04ca8d2fcaabd288c984cf698e3d829e05c134
Juan Cruz Leyba <jcruzleyba@rbi.com>
2022-07-19 19:58:58 -0300

    Updating to node imap module

--- app/services/imap.server.tsx
+++ app/services/imap.server.tsx
@@ -1,39 +1,103 @@
-//@ts-ignore
-import ImapClient from "emailjs-imap-client";
+import Imap from "imap";
+import { inspect } from "util";
 import isThisWeek from "date-fns/isThisWeek";
 
-var client = new ImapClient("imap.gmail.com", 993, {
-  auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-  },
+let imap = new Imap({
+  user: process.env.MAIL_USERNAME || "",
+  password: process.env.MAIL_PASSWORD || "",
+  host: "imap.gmail.com",
+  port: 993,
+  tls: true,
+  tlsOptions: { rejectUnauthorized: false },
 });
+
+function openInbox(cb: (error: Error, mailbox: Imap.Box) => void) {
+  imap.openBox("[Gmail]/Importantes", true, cb);
+}
+
+async function getLastEmail(): Promise<{
+  [index: string]: string[];
+} | null> {
+  try {
+    return new Promise((resolve, reject) => {
+      imap.once("ready", function () {
+        openInbox(function (err, box) {
+          if (err) {
+            return reject(err);
+          }
+          const f = imap.seq.fetch(box.messages.total + ":*", {
+            bodies: "HEADER.FIELDS (FROM TO SUBJECT DATE)",
+            struct: true,
+          });
+
+          let buffer = "";
+
+          f.on("message", function (msg, seqno) {
+            const prefix = "(#" + seqno + ") ";
+            msg.on("body", function (stream, info) {
+              stream.on("data", function (chunk) {
+                buffer += chunk.toString("utf8");
+              });
+              stream.once("end", function () {
+                console.log(
+                  prefix + "Parsed header: %s",
+                  inspect(Imap.parseHeader(buffer))
+                );
+              });
+            });
+
+            msg.once("end", function () {
+              console.log(prefix + "Finished");
+            });
+          });
+
+          f.once("error", function (err) {
+            console.log("Fetch error: " + err);
+            reject(err);
+          });
+
+          f.once("end", function () {
+            console.log("Done fetching all messages!");
+            imap.end();
+            resolve(Imap.parseHeader(buffer));
+          });
+        });
+      });
+
+      imap.once("error", function (err: Error) {
+        console.log(err);
+        reject(err);
+      });
+
+      imap.once("end", function () {
+        console.log("Connection ended");
+      });
+
+      imap.connect();
+    });
+  } catch (e) {
+    console.error("Something went wrong getting last email: ", e);
+    return null;
+  }
+}
 
 export async function hasMailBeenSent() {
   try {
-    await client.connect();
-
     console.log("Connected to IMAP");
 
-    const inbox = await client.selectMailbox("[Gmail]/Importantes");
+    const lastMessage = await getLastEmail();
 
-    const [lastMessage] = await client.listMessages(
-      "[Gmail]/Importantes",
-      inbox.exists,
-      ["envelope"]
-    );
+    const [date] = lastMessage?.date || [];
 
-    const date = lastMessage?.envelope?.date;
+    if (!date) return true;
+
     const hasBeenSent = isThisWeek(new Date(date), { weekStartsOn: 1 });
-    console.log("Last Message: ", lastMessage.envelope);
-
-    client.close();
+    console.log("Last Message: ", lastMessage);
 
     return hasBeenSent;
   } catch (e) {
-    console.error("Something went wrong getting last email");
+    console.error("Something went wrong getting last email: ", e);
   }
 
-  client.close();
   return true;
 }
